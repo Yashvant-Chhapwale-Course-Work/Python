c976fd75d0906c149a89cb44409aa31c45190c53
Yashvant_Chhapwale_Course_Work <coursework0603@gmail.com>
2024-09-30 07:34:15 +0530

    Day_1 - Day_2

--- Day_2/Mini_Challenge/BMI-calculator.py
+++ Day_2/Mini_Challenge/BMI-calculator.py
@@ -5,8 +5,8 @@ weight = input("Enter Your Weight in Kilograms:\n")
 height = input("Enter Your Height in Meters:\n")
 bmi = float(weight)/(float(height) ** 2)
 
-if bmi < 18.5:                                                    #In Python, 'if', 'elif', and 'else' statements are used for conditional execution of code blocks. They allow you to control the flow of the program by executing different code based on certain conditions.
-    print("BMI: " + str(bmi) + "\n")                              #Syntax:
+if bmi < 18.5:                                                    # In Python, 'if', 'elif', and 'else' statements are used for conditional execution of code blocks. They allow you to control the flow of the program by executing different code based on certain conditions.
+    print("BMI: " + str(bmi) + "\n")                              # Syntax:
     print("Alert! Your are Underweight!")                                 #if condition1:                                                              
 elif bmi >= 18.5 and bmi < 25:                                              #Block of code to execute if condition1 is True                                                              
     print("BMI: " + str(bmi) + "\n")                                      #elif condition2:                                                             
