452b81f5fd2b28485278dd0527ffd83ed8cba071
Yashvant Chhapwale <yash.chhapwale@gmail.com>
2024-11-03 23:01:19 +0530

    Day_9 - Day_11

--- Day_9/Day-9_Project/blind-auction-system.py
+++ Day_9/Day-9_Project/blind-auction-system.py
@@ -7,22 +7,27 @@ auction_record = {}
 while True:
     display_art()
     name = input("Enter Your/Organization's Name: ")
-    if name.lower() in ['stop','/s','terminate','/t',' ']:
+    if name.lower() in ['stop','/s','terminate','/t']:
         break
     elif name in auction_record :
+        os.system('cls' if os.name == 'nt' else 'clear') #Prevents the same bidder from updating bids!
         print ("You Have Already Placed Your Bid!")
         continue
     else:
         bid = input("Bidding Amount: ")
-        if bid.lower() in ['stop','/s','terminate','/t',' ']:
+        if bid.lower() in ['stop','/s','terminate','/t']:
             break
+        elif any(input in [""," "] for input in [name,bid]): #Prohibits Empty Submissions ("", " ")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("A field cannot be left blank!")
+            continue
         else:
             auction_record[name] = int(bid)
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear') #This code removes the previous output in the shell, before prompting for the next bid input.
 print(" ")
 
-if len(auction_record) > 0:
+if len(auction_record) > 0: 
     highest_bidder(auction_record)
 else:
     print("We Hope To See You Soon!")
